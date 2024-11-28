a61000779928e804eef4f4247dc8bdf3d34e66d1
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-11-28 17:19:09 +0100

    feat: add ticket price

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// - PROBLEM - 
+
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 // - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+// - STEPS -
 
 // 1. Chiedi all'utente di inserire il numero di chilometri che vuole percorrere
 // 2. Chiedi all'utente di inserire l'età del passeggero
@@ -15,10 +19,28 @@
 //          - Mostra messaggio "🎫 Costo biglietto ((0.21 € * n km) -40%) anzichè (0.21 € * n km) 🎟 - sconto OVER 65 applicato!"      
 // 4. Mostra messaggio "🧳 Buon viaggio!!! 🗺"
 
-// 1.
+
+// - CODES -
+
+// 1. Chiedi all'utente di inserire il numero di chilometri che vuole percorrere
+
 const distance = Number(prompt ("Inserisci i chilometri che vuoi percorrere!"));
 console.log (typeof distance, distance);
 
-// 2.
+// 2. Chiedi all'utente di inserire l'età del passeggero
+
 const passengerAge = Number(prompt ("Inserisci la tua età!"));
 console.log (typeof passengerAge, passengerAge);
+
+// 3. In base alle informazioni precedenti calcola il costo totale del biglietto (0.21 € al km)
+
+const ticketPrice = (0.21 * distance);
+ticketPrice.toFixed(2);
+console.log (typeof ticketPrice, ticketPrice);
+
+//          - Mostra messaggio "🎫 Costo biglietto (0.21 € * n km) 🎟"
+//      - SE età minore di 18 anni calcola 20% di sconto (-20% su costo totale)
+//          - Mostra messaggio "🎫 Costo biglietto ((0.21 € * n km) -20%) anzichè (0.21 € * n km) 🎟 - sconto MINORENNE applicato!"
+//      - ALTRIMENTI SE età maggiore o uguale a 65 anni calcola 40% di sconto (-40% su costo totale)
+//          - Mostra messaggio "🎫 Costo biglietto ((0.21 € * n km) -40%) anzichè (0.21 € * n km) 🎟 - sconto OVER 65 applicato!"      
+// 4. Mostra messaggio "🧳 Buon viaggio!!! 🗺"
