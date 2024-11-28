2feb01d7cd5a0ff62f2fdf2a1e41f3b5f6c55d66
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-11-28 16:35:53 +0100

    feat: add problem's steps

--- script.js
+++ script.js
@@ -4,3 +4,14 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 // - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+// 1. Chiedi all'utente di inserire il numero di chilometri che vuole percorrere
+// 2. Chiedi all'utente di inserire l'età del passeggero
+// 3. In base alle informazioni precedenti calcola il costo totale del biglietto (0.21 € al km)
+//          - Mostra messaggio "🎫 Costo biglietto (0.21 € * n km) 🎟"
+//      - SE età minore di 18 anni calcola 20% di sconto (-20% su costo totale)
+//          - Mostra messaggio "🎫 Costo biglietto ((0.21 € * n km) -20%) anzichè (0.21 € * n km) 🎟 - sconto MINORENNE applicato!"
+//      - ALTRIMENTI SE età maggiore o uguale a 65 anni calcola 40% di sconto (-40% su costo totale)
+//          - Mostra messaggio "🎫 Costo biglietto ((0.21 € * n km) -40%) anzichè (0.21 € * n km) 🎟 - sconto OVER 65 applicato!"      
+// 4. Mostra messaggio "🧳 Buon viaggio!!! 🗺"
+
